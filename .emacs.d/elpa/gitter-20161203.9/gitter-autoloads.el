9e39a4152e62e840777874abd86032f4e938b706
James Kent <jameschristopherkent@gmail.com>
2017-10-19 17:34:22 +0100

    Added .emacs.d

---
+++ .emacs.d/elpa/gitter-20161203.9/gitter-autoloads.el
@@ -0,0 +1,25 @@
+;;; gitter-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "gitter" "gitter.el" (22907 16790 0 0))
+;;; Generated autoloads from gitter.el
+
+(autoload 'gitter "gitter" "\
+Open a room.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("gitter-pkg.el") (22907 16789 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; gitter-autoloads.el ends here
