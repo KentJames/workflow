8b9222296744754ca8fbdafa1b3c61d33862f722
James Kent <jameschristopherkent@gmail.com>
2017-12-24 00:37:36 +0000

    Emacs workflow update

--- .emacs.d/elpa/org-random-todo-20170404.318/org-random-todo-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-random-todo" "20170404.318" "notify of random TODO's" '((emacs "24.3") (alert "1.2")) :commit "8b784ff0419fcaa2467991bc670e0e8e31be62c0" :keywords '("org" "todo" "notification"))
